00557dad8524b1676da2a5a05d90dd06a7a69b83
koogk7 <koogk7@gmail.com>
2019-12-29 17:28:54 +0900

    Simple Web Sever

---
+++ WebServer/main/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// 두번째 인자는 port 번호
+var addr = flag.String("addr", ":8080", "http service address")
+var temple = template.Must(template.New("qr").Parse(templateStr)) // HTML 템플릿 제작
+
+func main()  {
+	// HTTP 포트 세팅
+	flag.Parse()
+
+	// URL 바인딩
+	http.Handle("/", http.HandlerFunc(QR))
+
+	//
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func QR(w http.ResponseWriter, req *http.Request) {
+	// HTML 템플릿 렌더링
+	_ = temple.Execute(w, req.FormValue("s"))
+}
+
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
